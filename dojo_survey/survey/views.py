647fefe7c647c142dc08e6bd8f6675943ffce63c
Rami ALAllam <rami_alam_1999@hotmail.com>
2023-11-09 21:30:00 +0200

    ASS3--

--- dojo_survey/survey/views.py
+++ dojo_survey/survey/views.py
@@ -5,13 +5,17 @@ def survey(request):
 
 def result(request):
     name1 = request.POST['name']
+    gender1 = request.POST['gender'] 
     city1 = request.POST['city']
     lang1 = request.POST['lang']
     comment1 = request.POST['comment']
+    offer1 = request.POST['offers']
     context = {
         "name": name1,
+        "gender": gender1,
         "city": city1,
         "lang": lang1,
-        "comment": comment1
+        "comment": comment1,
+        "offers" : offer1
     }
     return render(request,"result.html",context)
